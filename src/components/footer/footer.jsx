767c7b0a050241154108aa0a1b892aeb23e6595c
Gianella Meringolo <gmeringolo@gmail.com>
2024-03-29 15:00:51 -0300

    arreglo redes y wup

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -15,13 +15,13 @@ function Footer() {
             <img src={logo} alt="Viviana Castro" />
           </a>
           <div className={styles.contacto_redes}>
-            <a href="https://www.instagram.com/posadahorizonte/" target="_blanck" className={styles.instagram}>
+            <a href="https://www.instagram.com/viviana.castrosequeira" target="_blanck" className={styles.instagram}>
               <img src={Ig} alt="Instagram" />
             </a>
-            <a href="https://www.facebook.com/posadahorizonte" target="_blanck" className={styles.facebook}>
+            <a href="https://www.facebook.com/viviana.castrosequeira" target="_blanck" className={styles.facebook}>
               <img src={Face} alt="facebook" />
             </a>
-            <a href="https://wa.me/+59899667726?text=Para%20consultar%20disponibilidad,%20indicar%20fecha%20de%20entrada%20y%20salida" target="_blanck" className={styles.whatsapp}>
+            <a href="https://wa.me/+59898873697?text=Para%20consultar%20disponibilidad,%20indicar%20fecha%20de%20entrada%20y%20salida" target="_blanck" className={styles.whatsapp}>
               <img src={Whats} alt="whatsapp" />
             </a>
           </div>
